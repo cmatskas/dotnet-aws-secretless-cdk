3e2468fd53da39e2a2d3cb6740f398632c0400a1
Matskas <christos.matskas@gmail.com>
2022-08-31 19:30:31 -0400

    Initial commit

---
+++ src/DotnetAwsSecureCdk/DotnetAwsSecureCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace DotnetAwsSecureCdk
+{
+    public class DotnetAwsSecureCdkStack : Stack
+    {
+        internal DotnetAwsSecureCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
