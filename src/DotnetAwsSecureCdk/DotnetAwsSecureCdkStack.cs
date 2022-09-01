7be26c37e2e2eec2507f3a278d4f457eb4746a1b
Matskas <christos.matskas@gmail.com>
2022-09-01 13:54:58 -0400

    AWS Lamdba Function with secure Env Variable

--- src/DotnetAwsSecureCdk/DotnetAwsSecureCdkStack.cs
+++ src/DotnetAwsSecureCdk/DotnetAwsSecureCdkStack.cs
@@ -1,13 +1,33 @@
 using Amazon.CDK;
-using Constructs;
+using Amazon.CDK.AWS.Events;
+using Amazon.CDK.AWS.Events.Targets;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.SecretsManager;
 
 namespace DotnetAwsSecureCdk
 {
     public class DotnetAwsSecureCdkStack : Stack
     {
-        internal DotnetAwsSecureCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        public DotnetAwsSecureCdkStack(Amazon.CDK.Construct scope, string id, IStackProps? props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            var secret = Secret.FromSecretNameV2(this, id, "ApiKey");
+
+            var lambdaFn = new Function(this, "Singleton", new FunctionProps {
+                Runtime = Runtime.PYTHON_3_9,
+                Code = Code.FromInline($"def main(event, context):{System.Environment.NewLine}    print(\"Do something with the ApiKey \"){System.Environment.NewLine}"),
+                Handler = "index.main",
+                Timeout = Duration.Seconds(300),
+            });
+
+            lambdaFn.AddEnvironment("ApiKey", secret.SecretValue.UnsafeUnwrap());
+
+    
+            var rule = new Rule(this, "Rule", new RuleProps {
+                Schedule = Schedule.Expression("cron(0 18 ? * MON-FRI *)"),
+            });
+
+            rule.AddTarget(new LambdaFunction(lambdaFn));
         }
+            
     }
 }
